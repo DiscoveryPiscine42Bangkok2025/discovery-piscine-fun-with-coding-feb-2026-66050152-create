afc2380dec857e34b642a32ae8796e02bc4a98ae
66050152-create <66050152@kmitl.ac.th>
2026-02-07 11:41:37 +0700

    submit

---
+++ cell00/ex03/count_files.sh
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <title>Piscine Web</title>
+</head>
+<body>
+
+    <h1>Piscine Web</h1>
+
+    <img src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=500" alt="Cute Cat 1">
+
+    <br> <img src="c:\Users\ASus\OneDrive\Pictures\Meme-01.jpeg" alt="Cute Cat 2">
+
+</body>
+</html>
